b163cde2b1a1486a8473ad455af7d6d546c0fd9e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-12-30 23:25:55 -0500

    Import Home and add route to home page

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes as Switch } from 'react-router-dom';
+import Home from './components/pages/Home';
+// import Products from './components/pages/Products';
+// import Services from './components/pages/Services';
+// import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
     <>
-    
+      <Router>
+        {/* <Navbar /> */}
+        <Switch>
+          <Route path='/' exact element={< Home />}></Route>
+          {/* <Route path='/services' exact element={< Services />}></Route>
+          <Route path='/products' exact element={< Products />}></Route>
+          <Route path='/sign-up' exact element={< SignUp />}></Route> */}
+        </Switch>
+      </Router>
     </>
   );
 }
