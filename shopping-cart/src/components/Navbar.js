25e7aaa205c7606a82e57989a36ac899dadc914f
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-01 19:00:04 -0500

    Lik CSS file

--- shopping-cart/src/components/Navbar.js
+++ shopping-cart/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-// import './Navbar.css';
+import './Navbar.css';
 import { Button } from './Button';
 
 function Navbar() {
