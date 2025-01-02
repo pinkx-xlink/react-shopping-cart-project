b3568d6bf4c7fdbb45efac54e1be58a94920aedf
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-01 20:37:28 -0500

    Create items array (items will be replaced later by products from an API)

--- shopping-cart/src/components/pages/Shop.js
+++ shopping-cart/src/components/pages/Shop.js
@@ -3,10 +3,28 @@ import '../../App.css';
 import Cards from '../Cards';
 
 export default function Shop() {
+  const items = [
+    {
+      id: 1,
+      name: "overwatch",
+      price: 20,
+    },
+    {
+      id: 2,
+      name: "minecraft",
+      price: 32,
+    },
+    {
+      id: 3,
+      name: "fortnite",
+      price: 51,
+    },
+  ];
     return (
         <>
           <h1 className='shop'>SHOP</h1>
           < Cards />
+          <div>{items[0].name}</div>
         </>
     )
 }
