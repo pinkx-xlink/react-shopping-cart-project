4dfe560a33932df8708c4287045d4a38f0979d4c
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-27 23:55:41 -0500

    Update text

--- shopping-cart/src/components/Cards.js
+++ shopping-cart/src/components/Cards.js
@@ -4,22 +4,29 @@ import './Cards.css'
 // import WaterfallImg from "src/images/waterfall-img.jpg";
 
 function Cards() {
+    
   return (
     <div className="cards">
         <h1>Check out these epic destinations!</h1>
         <div className="cards__container">
             <div className="cards__wrapper">
                 <ul className="cards__items">
+                <CardItem 
+                    src="images/Filler.png"
+                    text="Item 1 description text:"
+                    label="Beauty"
+                    path="/shop"
+                    />
                     <CardItem 
                     src="images/Filler.png"
-                    text="Explore the hidden waterfall deep within the Amazon Jungle"
-                    label="Adventure"
+                    text="Item 2 description text:"
+                    label="Womens Fashion"
                     path="/shop"
                     />
                      <CardItem 
                     src="images/Filler.png"
-                    text="Explore the hidden waterfall deep within the Amazon Jungle"
-                    label="Adventure"
+                    text="Item 3 description text:"
+                    label="Men's Fashion"
                     path="/shop"
                     />
                 </ul>
@@ -27,20 +34,20 @@ function Cards() {
                 <ul className="cards__items">
                     <CardItem 
                     src="images/Filler.png"
-                    text="Travel through the Islands of Balie in a Private Cruise"
-                    label="Luxury"
+                    text="Item 4 description text:"
+                    label="Bedroom Decor"
                     path="/shop"
                     />
                      <CardItem 
                     src="images/Filler.png"
-                    text="Travel through the Islands of Balie in a Private Cruise"
-                    label="Luxury"
+                    text="Item 5 description text:"
+                    label="Home Decor"
                     path="/shop"
                     />
                      <CardItem 
                     src="images/Filler.png"
-                    text="Travel through the Islands of Balie in a Private Cruise"
-                    label="Luxury"
+                    text="Item 6 description text:"
+                    label="Bathroom Decor"
                     path="/shop"
                     />
                 </ul>
