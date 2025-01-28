659d3c220f28374d296d2f89f16f5e39845e3073
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-28 01:04:53 -0500

    Tweak CSS and resize product cards

--- shopping-cart/src/components/Products.js
+++ shopping-cart/src/components/Products.js
@@ -2,7 +2,8 @@ import React, {useEffect, useState} from "react";
 import '../App.css';
 import axios from "axios";
 import './Products.css';
-
+import CardItem from "./CardItem";
+import Cards from "./Cards";
 const Products = () => {
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState([]);
@@ -37,25 +38,13 @@ const Products = () => {
                             <ul className="cards__items">
                             <CardItem 
                                 src="images/Filler.png"
-                                text="Item 1 description text:"
+                                text={product.description}
                                 label="Beauty"
-                                path="/shop"
-                                />
-                                <CardItem 
-                                src="images/Filler.png"
-                                text="Item 2 description text:"
-                                label="Womens Fashion"
-                                path="/shop"
-                                />
-                                <CardItem 
-                                src="images/Filler.png"
-                                text="Item 3 description text:"
-                                label="Men's Fashion"
                                 path="/shop"
                                 />
                             </ul>
                             {/* copy and paste card component */}
-                            <ul className="cards__items">
+                            {/* <ul className="cards__items">
                                 <CardItem 
                                 src="images/Filler.png"
                                 text="Item 4 description text:"
@@ -74,7 +63,7 @@ const Products = () => {
                                 label="Bathroom Decor"
                                 path="/shop"
                                 />
-                            </ul>
+                            </ul> */}
                         </div>
                     </div>
                     <p> {product.price} </p>
