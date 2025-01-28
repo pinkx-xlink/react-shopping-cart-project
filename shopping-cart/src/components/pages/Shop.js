64c6371173635f73bbb3b63b68ca5daa472629e2
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-28 15:01:19 -0500

    Delete extra spaces and unnecessary import

--- shopping-cart/src/components/pages/Shop.js
+++ shopping-cart/src/components/pages/Shop.js
@@ -1,17 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import '../../App.css';
 import Products from '../Products';
-import axios
- from 'axios';
+
 const Shop = () => {
- 
-    return (
-        <>
-          <h1 className='shop'>SHOP</h1>
-          < Products />
-        
-        </>
-    )
+  return (
+    <>
+      <h1 className='shop'>SHOP</h1>
+      < Products />
+    </>
+  )
 }
 
 export default Shop
