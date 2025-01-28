e8ee8ce8b010b8cc2218f463cd3bf1b9308b63bc
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-28 00:58:35 -0500

    Copy and paste Cards.css code to Products.css

--- shopping-cart/src/components/Products.js
+++ shopping-cart/src/components/Products.js
@@ -32,6 +32,51 @@ const Products = () => {
 
             {data.map((product)=> (
                 <div key={product.id} className="product-card">
+                    <div className="cards__container">
+                        <div className="cards__wrapper">
+                            <ul className="cards__items">
+                            <CardItem 
+                                src="images/Filler.png"
+                                text="Item 1 description text:"
+                                label="Beauty"
+                                path="/shop"
+                                />
+                                <CardItem 
+                                src="images/Filler.png"
+                                text="Item 2 description text:"
+                                label="Womens Fashion"
+                                path="/shop"
+                                />
+                                <CardItem 
+                                src="images/Filler.png"
+                                text="Item 3 description text:"
+                                label="Men's Fashion"
+                                path="/shop"
+                                />
+                            </ul>
+                            {/* copy and paste card component */}
+                            <ul className="cards__items">
+                                <CardItem 
+                                src="images/Filler.png"
+                                text="Item 4 description text:"
+                                label="Bedroom Decor"
+                                path="/shop"
+                                />
+                                <CardItem 
+                                src="images/Filler.png"
+                                text="Item 5 description text:"
+                                label="Home Decor"
+                                path="/shop"
+                                />
+                                <CardItem 
+                                src="images/Filler.png"
+                                text="Item 6 description text:"
+                                label="Bathroom Decor"
+                                path="/shop"
+                                />
+                            </ul>
+                        </div>
+                    </div>
                     <p> {product.price} </p>
                     <p> {product.title} </p>
                     <p> {product.description} </p>
