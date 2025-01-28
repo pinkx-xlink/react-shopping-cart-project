0104c1be8e532fe4599f6b25a9d793ec494cf8cf
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-28 02:54:29 -0500

    Remove shopping cart logic from Shop const

--- shopping-cart/src/components/pages/Shop.js
+++ shopping-cart/src/components/pages/Shop.js
@@ -4,73 +4,12 @@ import Products from '../Products';
 import axios
  from 'axios';
 const Shop = () => {
-  const [cart, setCart] = useState([]);
-  const [cartTotal, setCartTotal] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-      setLoading(true);
-
-      axios({
-          method:"GET",
-          url:"https://fakestoreapi.com/products"
-      })
-      .then(res => {
-          console.log(res.data)
-          setData(res.data)
-      })
-      .catch((e) => console.log(e))
-      .finally(() => setLoading(false));
-  }, []);
-
-  // Refactor this to use res.data products.id, title, and 
-  // price instead of static array
-
-  const items = data
-
-  const listItems = items.map((el) => (
-    <div key={el.id}>
-      {`${el.title}: $${el.price}`}
-      <input type="submit" value="add" onClick={() => addToCart(el)} />
-    </div>
-  ))
-
-  useEffect(() => {
-    total();
-  }, [cart]);
-  
-  const total = () => {
-    let totalVal = 0;
-    for (let i = 0; i < cart.length; i++) {
-      totalVal += cart[i].price;
-    }
-    setCartTotal(totalVal);
-  };
-
-  const addToCart = (el) => {
-    setCart([...cart, el]);
-  };
-
-  const removeFromCart = (el) => {
-    let hardCopy = [...cart];
-    hardCopy = hardCopy.filter((cartItem) => cartItem.id !== el.id);
-    setCart(hardCopy);
-  };
-
-  const cartItems = cart.map((el) => (
-    <div key={el.id}>
-      {`${el.title}: $${el.price}`}
-      <input type="submit" value="remove" onClick={() => removeFromCart(el)} />
-    </div>
-  ));
+ 
     return (
         <>
           <h1 className='shop'>SHOP</h1>
           < Products />
-          <div>{listItems}</div>
-          <div>CART</div>
-          <div>{cartItems}</div>
+        
         </>
     )
 }
