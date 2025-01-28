5bfd9ae8a62874e60601d779449feb93ad3e07f7
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-28 14:53:57 -0500

    Delete unnecessary spaces

--- shopping-cart/src/components/Products.js
+++ shopping-cart/src/components/Products.js
@@ -7,91 +7,90 @@ import Cards from "./Cards";
 import { Link } from "react-router-dom";
 
 const Products = () => {
-    const [cart, setCart] = useState([]);
-    const [cartTotal, setCartTotal] = useState(0);
+  const [cart, setCart] = useState([]);
+  const [cartTotal, setCartTotal] = useState(0);
+  
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    axios({
+      method:"GET",
+      url:"https://fakestoreapi.com/products"
+    })
+    .then(res => {
+      console.log(res.data)
+      setData(res.data)
+    })
+    .catch((e) => console.log(e))
+    .finally(() => setLoading(false));
+  }, []);
     
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState([]);
+  const items = data
 
-    useEffect(() => {
-        setLoading(true);
+  const listItems = items.map((product) => (
+    <div key={product.id}>
+      {`${product.title}: $${product.price}`}
+      <input type="submit" value="add" onClick={() => addToCart(product)} />
+    </div>
+  ))
 
-        axios({
-            method:"GET",
-            url:"https://fakestoreapi.com/products"
-        })
-        .then(res => {
-            console.log(res.data)
-            setData(res.data)
-        })
-        .catch((e) => console.log(e))
-        .finally(() => setLoading(false));
-    }, []);
-    
-const items = data
+  useEffect(() => {
+    total();
+  }, [cart]);
 
-const listItems = items.map((product) => (
-  <div key={product.id}>
-    {`${product.title}: $${product.price}`}
-    <input type="submit" value="add" onClick={() => addToCart(product)} />
-  </div>
-))
+  const total = () => {
+    let totalVal = 0;
+    for (let i = 0; i < cart.length; i++) {
+      totalVal += cart[i].price;
+    }
+    setCartTotal(totalVal);
+  };
 
-useEffect(() => {
-  total();
-}, [cart]);
+  const addToCart = (product) => {
+    setCart([...cart, product]);
+  };
 
-const total = () => {
-  let totalVal = 0;
-  for (let i = 0; i < cart.length; i++) {
-    totalVal += cart[i].price;
-  }
-  setCartTotal(totalVal);
-};
+  const removeFromCart = (product) => {
+    let hardCopy = [...cart];
+    hardCopy = hardCopy.filter((cartItem) => cartItem.id !== product.id);
+    setCart(hardCopy);
+  };
 
-const addToCart = (product) => {
-  setCart([...cart, product]);
-};
+  const cartItems = cart.map((product) => (
+    <div key={product.id}>
+      {`${product.title}: $${product.price}`}
+      <input type="submit" value="remove" onClick={() => removeFromCart(product)} />
+    </div>
+  ));
 
-const removeFromCart = (product) => {
-  let hardCopy = [...cart];
-  hardCopy = hardCopy.filter((cartItem) => cartItem.id !== product.id);
-  setCart(hardCopy);
-};
+  return (
+    <>
+      <div>{cartItems}</div>
+      <div className='products-container'>
+          {loading && (
+            <div> 
+              {" "}
+              <h1> Loading... </h1> 
+            </div>
+          )}
 
-const cartItems = cart.map((product) => (
-  <div key={product.id}>
-    {`${product.title}: $${product.price}`}
-    <input type="submit" value="remove" onClick={() => removeFromCart(product)} />
-  </div>
-));
-
-    return (
-        <>
-        <div>{cartItems}</div>
-        <div className='products-container'>
-            {loading && (
-                <div> 
-                    {" "}
-                    <h1> Loading... </h1> 
-                </div>
-            )}
-
-            {data.map((product)=> (
-                //  <Link className="cards__item__link" to={props.path}></Link>
-                <div key={product.id} className="card">
-                    <Link className="link__to__shop" to="/shop"><img src={product.image} alt="#"/></Link>
-                    <div className="card-description">
-                        <h2>{product.title}</h2>
-                        <h2> ${product.price} </h2>
-                        <h6> Descrption: {product.description} </h6>
-                        <input input type="submit" value="add" onClick={() => addToCart(product)} />
-                    </div>
-                </div>   
-            ))}
+          {data.map((product)=> (
+            <div key={product.id} className="card">
+              <Link className="link__to__shop" to="/shop"><img src={product.image} alt="#"/></Link>
+              <div className="card-description">
+                <h2>{product.title}</h2>
+                <h2> ${product.price} </h2>
+                <h6> Descrption: {product.description} </h6>
+                <input input type="submit" value="add" onClick={() => addToCart(product)} />
+              </div>
+            </div>   
+          ))}
         </div>
-        </>
-    );
+    </>
+  );
 };
 
 export default Products
