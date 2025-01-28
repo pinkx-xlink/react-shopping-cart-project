7b1a347155cbc766b1bb2c7802ba01d5c75796e8
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-28 00:05:43 -0500

    Create Products.js

---
+++ shopping-cart/src/components/Products.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+import '../App.css';
+import 'axios' from axios;
+
+cost Products = () => {
+    return (
+        <div className='products-container'>Products</div>
+    )
+}
