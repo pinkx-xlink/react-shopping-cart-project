7e6f33b12bfef968e24d7d32d1c3c4e445d5e813
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-01-30 19:33:00 -0500

    Create simple RenderProducts class

---
+++ shopping-cart/src/components/RenderProducts.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class RenderProducts extends Component {
+   render() {
+        return (
+            <li>
+                <input> </input>
+                <span> </span>
+                <button> </button>
+            </li>
+        );
+   }
+}
+
