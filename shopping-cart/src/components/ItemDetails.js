ea65f27816489f77795a2844819167212e23fe29
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-10 22:28:31 -0500

    Update all instances of 'description' to 'title'

--- shopping-cart/src/components/ItemDetails.js
+++ shopping-cart/src/components/ItemDetails.js
@@ -3,8 +3,8 @@ import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom';
 
 function ItemDetail({ handleClick }) {
-    const { id, price, description } = useParams();
-    let item = {id, price, description};
+    const { id, price, title } = useParams();
+    let item = {id, price, title};
 
     console.log(item);
 
@@ -13,7 +13,7 @@ function ItemDetail({ handleClick }) {
             <div>
                 <p>Product id: {id}</p>
                 <p>price: {price}</p>
-                <p>description: {description}</p>
+                <p>item: {title}</p>
                 <button onClick={() => handleClick(item)}> Add to Cart </button>
                 <Link to="/shop">exit</Link>
             </div>
