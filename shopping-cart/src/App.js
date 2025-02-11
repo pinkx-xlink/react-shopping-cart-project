fb0bab5a44043b4a4730ad3c57f804abc42e3f1d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-10 20:21:08 -0500

    Remove cart logic from App.js and return it to Shop.js

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -13,47 +13,7 @@ import Cart from '../src/components/Cart';
 
 
 function App() {
-  const [show, setShow] = useState(true);
-  const [cart, setCart] = useState([]);
-
-  const handleClick = (item) => {
-    // Update cart item qty if already in the cart
-    if (cart.some((cartItem) => cartItem.id === item.id)) {
-      setCart((cart) => 
-      cart.map((cartItem) =>
-        cartItem.id === item.id
-      ? {
-        ...cartItem,
-        amount: cartItem.amount + 1
-      } 
-    : cartItem
-  )
-);
-return;
-    }
-    // Add to cart
-    setCart((cart) => [
-      ...cart,
-      { ...item, amount: 1 }
-    ]);
-  };
-  
-  const handleChange = (id, d) => {
-    setCart((cart) =>
-      cart.flatMap((cartItem) => 
-        cartItem.id === id
-          ? cartItem.amount + d < 1
-          ? [] // remove item if amount will be less than 1
-          : [
-              {
-                ...cartItem,
-                amount: cartItem.amount + d
-              }
-            ]
-          : [cartItem]
-        )
-      );
-    };
+ 
 
   return (
       <Router>
--- shopping-cart/src/components/pages/Shop.js
+++ shopping-cart/src/components/pages/Shop.js
@@ -6,7 +6,47 @@ import Cart from '../Cart';
 import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 
 const Shop = () => {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
 
+  const handleClick = (item) => {
+    // Update cart item qty if already in the cart
+    if (cart.some((cartItem) => cartItem.id === item.id)) {
+      setCart((cart) => 
+      cart.map((cartItem) =>
+        cartItem.id === item.id
+      ? {
+        ...cartItem,
+        amount: cartItem.amount + 1
+      } 
+    : cartItem
+  )
+);
+return;
+    }
+    // Add to cart
+    setCart((cart) => [
+      ...cart,
+      { ...item, amount: 1 }
+    ]);
+  };
+  
+  const handleChange = (id, d) => {
+    setCart((cart) =>
+      cart.flatMap((cartItem) => 
+        cartItem.id === id
+          ? cartItem.amount + d < 1
+          ? [] // remove item if amount will be less than 1
+          : [
+              {
+                ...cartItem,
+                amount: cartItem.amount + d
+              }
+            ]
+          : [cartItem]
+        )
+      );
+    };
 
 
   return (
