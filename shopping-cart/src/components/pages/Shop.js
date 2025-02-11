b3e632b62c44cf2425e703921532c574c012e373
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-10 19:28:20 -0500

    Import ItemDetail and Cart to Shop.js

--- shopping-cart/src/components/pages/Shop.js
+++ shopping-cart/src/components/pages/Shop.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import '../../App.css';
 import Products from '../Products';
+import ItemDetail from '../ItemDetails';
+import Cart from '../Cart';
 
 const Shop = () => {
   return (
