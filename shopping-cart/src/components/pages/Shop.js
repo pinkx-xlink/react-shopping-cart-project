9a649e218c4e22cf410893433d780d80cc568334
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-02-10 19:35:48 -0500

    Add handleClick to Shop()

--- shopping-cart/src/components/pages/Shop.js
+++ shopping-cart/src/components/pages/Shop.js
@@ -5,6 +5,23 @@ import ItemDetail from '../ItemDetails';
 import Cart from '../Cart';
 
 const Shop = () => {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+  const handleClick = (item) => {
+    // Update cart item qty if already in the cart
+    if (cart.some((cartItem) => cartItem.id === item.id)) {
+      setCart((cart) => 
+      cart.map((cartItem) =>
+        cartItem.id === item.id
+      ? {
+        ...cartItem,
+        amount: cartItem.amount + 1
+      } 
+    : cartItem
+  )
+);
+    }
+  }
   return (
     <>
       <h1 className='shop'>SHOP</h1>
