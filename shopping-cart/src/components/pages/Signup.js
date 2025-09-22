44fa841fef973456186dd19bf1650902e9b1fc0b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-22 18:50:07 -0400

    Add header

--- shopping-cart/src/components/pages/Signup.js
+++ shopping-cart/src/components/pages/Signup.js
@@ -1,8 +1,23 @@
 import React from 'react'
 
 const Signup = () => {
+    
   return (
-    <div>Signup</div>
+    <div>
+        <h1>Sign Up</h1>
+         <header>
+        <h2 class="logo">MyWebsite</h2>
+        <nav class="navigation">
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="services.html">Services</a>
+            <a href="contact.html">Contact</a>
+            <button class="btnLogin-popup">Login</button>
+        </nav>
+
+    </header>
+
+    </div>
   )
 }
 
