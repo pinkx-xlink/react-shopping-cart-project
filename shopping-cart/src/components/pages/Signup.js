8966ff27ca03b789a46c541148b753cc87c474e2
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-22 19:09:43 -0400

    Change all instances of class to className

--- shopping-cart/src/components/pages/Signup.js
+++ shopping-cart/src/components/pages/Signup.js
@@ -1,45 +1,69 @@
-import React from 'react'
+import React from 'react';
+import './signup.css';
+import background from './background.jpg';
 
 const Signup = () => {
-    console.log('m')
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+    const btnPopup = document.querySelector('.btnLogin-popup');
+    const iconClose = document.querySelector('.icon-close');
+
+
+    registerLink.addEventListener('click', () => {
+        wrapper.classList.add('active');
+    });
+
+    loginLink.addEventListener('click', () => {
+        wrapper.classList.remove('active');
+    });
+
+    btnPopup.addEventListener('click', () => {
+        wrapper.classList.add('active-popup');
+    });
+
+    iconClose.addEventListener('click', () => {
+        wrapper.classList.remove('active-popup');
+    });
+
   return (
     <>
       <header>
-            <h2 class="logo">MyWebsite</h2>
-            <nav class="navigation">
+            <h2 className="logo">MyWebsite</h2>
+            <nav className="navigation">
                 <a href="index.html">Home</a>
                 <a href="about.html">About</a>
                 <a href="services.html">Services</a>
                 <a href="contact.html">Contact</a>
-                <button class="btnLogin-popup">Login</button>
+                <button className="btnLogin-popup">Login</button>
             </nav>
         </header>
 
-        <div class="wrapper">
-            <span class="icon-close">
+        <div className="wrapper">
+            <span className="icon-close">
                 <ion-icon name="close"></ion-icon>
             </span>
 
-            <div class="form-box login">
+            <div className="form-box login">
                 <h2>Login</h2>
                 <form action="#">
-                    <div class="input-box">
-                        <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                    <div className="input-box">
+                        <span className="icon"><ion-icon name="mail"></ion-icon></span>
                         <input type="email" required/>
                         <label>Email</label>
                     </div>
-                    <div class="input-box">
-                        <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <div className="input-box">
+                        <span className="icon"><ion-icon name="lock-closed"></ion-icon></span>
                         <input type="password" required/>
                         <label>Password</label>
                     </div>
-                    <div class="remember-forgot">
+                    <div className="remember-forgot">
                         <label><input type="checkbox"/>Remember me</label>
                         <a href="#">Forgot Password?</a>
                     </div>
-                    <button type="submit" class="btn">Login</button>
-                    <div class="login-register">
-                        <p>Don't have an account? <a href="#" class="register-link">Register</a>
+                    <button type="submit" className="btn">Login</button>
+                    <div className="login-register">
+                        <p>Don't have an account? <a href="#" className="register-link">Register</a>
                         </p>
                     </div>
                 </form>
@@ -49,30 +73,30 @@ const Signup = () => {
             <div class="form-box register">
                 <h2>Registration</h2>
                 <form action="#">
-                    <div class="input-box">
-                        <span class="icon"><ion-icon name="person"></ion-icon></span>
+                    <div className="input-box">
+                        <span className="icon"><ion-icon name="person"></ion-icon></span>
                         <input type="text" required/>
                         <label>Username</label>
                     </div>
-                    <div class="input-box">
-                        <span class="icon"><ion-icon name="mail"></ion-icon></span>
+                    <div className="input-box">
+                        <span className="icon"><ion-icon name="mail"></ion-icon></span>
                         <input type="email" required/>
                         <label>Email</label>
                     </div>
-                    <div class="input-box">
-                        <span class="icon"><ion-icon name="lock-closed"></ion-icon></span>
+                    <div className="input-box">
+                        <span className="icon"><ion-icon name="lock-closed"></ion-icon></span>
                         <input type="password" required/>
                         <label>Password</label>
                     </div>
-                    <div class="remember-forgot">
+                    <div className="remember-forgot">
                         <label><input type="checkbox"/>
                         I agree to the terms & conditions
                         </label>
                         <a href="#">Forgot Password?</a>
                     </div>
-                    <button type="submit" class="btn">Register</button>
-                    <div class="login-register">
-                        <p>Already have an account? <a href="#" class="login-link">Login</a>
+                    <button type="submit" className="btn">Register</button>
+                    <div className="login-register">
+                        <p>Already have an account? <a href="#" className="login-link">Login</a>
                         </p>
                     </div>
                 </form>
