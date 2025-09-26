108458f337a61724d42c795774d7b0a340a3da52
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-26 13:34:21 -0400

    Stylize the product card items (background color, content alignment, sizing, etc.)

--- shopping-cart/src/components/Navbar.js
+++ shopping-cart/src/components/Navbar.js
@@ -89,7 +89,7 @@ function Navbar() {
                         onClick={closeMobileMenu}
                     >Login 
                     </Link>
-                    </Button>}
+                </Button>}
             </div>
         </nav>
     </>
