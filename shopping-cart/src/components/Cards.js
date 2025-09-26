5612f1632aa544a92308c19db142b1bd853013f0
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-26 16:48:20 -0400

    Comment out cards temporarily and add boilerplate image sildeshow code

--- shopping-cart/src/components/Cards.js
+++ shopping-cart/src/components/Cards.js
@@ -29,7 +29,7 @@ const Cards = () => {
     let currentSlide = 0;
 
     function showSlide(index) {
-        const slides = document.querySelectorAll('.cards');
+        const slides = document.querySelectorAll('.slideshow-container .slide');
 
         slides.forEach((cards, i) => {
             cards.style.display = i === index ? 'flex' : 'none';
@@ -37,7 +37,7 @@ const Cards = () => {
     }
 
     function nextSlide() {
-        currentSlide = (currentSlide + 1) % 6;
+        currentSlide = (currentSlide + 1) % 3;
         showSlide(currentSlide);
     }
 
@@ -46,6 +46,39 @@ const Cards = () => {
 
   return (
     <>
+
+
+    <div className="slideshow-container">
+		<div className="slide fade">
+			<img src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20231126110234/ezgifcom-gif-maker-1.webp"
+				alt="Slide 1"/>
+		</div>
+		<div className="slide fade">
+			<img src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20231217134958/Group-23.png" alt="Slide 2"/>
+		</div>
+		<div className="slide fade">
+			<img src=	 
+"https://media.geeksforgeeks.org/wp-content/uploads/20240106180224/Screenshot-2024-01-06-174436.gif"
+				alt="Slide 3"/>
+		</div>
+		<div className="slide fade">
+			<img src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20240109190054/Screenshot-2024-01-09-185442.gif"
+				alt="Slide 3"/>
+		</div>
+        <div className="slide fade">
+			<img src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20231123180952/Group-18.png" alt="Slide 3"/>
+		</div>
+		<div className="slide fade">
+			<img src=
+"https://media.geeksforgeeks.org/wp-content/uploads/20231121180023/Group-17.png" alt="Slide 3"/>
+		</div>
+	</div>
+
+
     <div className='products-container'>
         {loading && (
             <div> 
@@ -54,30 +87,33 @@ const Cards = () => {
             </div>
         )}
 
-<div className='product-cards-container'>
-{data.slice(0, 3).map((product)=> (
-    
-    <div key={product.id} className="cards fade">
-        {/* <h2>{product.title}</h2> */}
-        <div className="cards__container">
-            <div className="cards__wrapper">
-                <ul className="cards__items">
-                <CardItem 
-                    src={product.image}
-                    text={product.title}
-                    label="New!"
-                    path="/shop"
-                    />
-                </ul>
-            </div>
-        </div>
-    </div>
-    
-    ))}
-    </div>
+
     </div>
     </>
   )
 }
 
 export default Cards;
+
+
+{/* <div className='product-cards-container'>
+{data.slice(0, 3).map((product)=> (
+    
+    <div key={product.id} className="cards">
+        {/* <h2>{product.title}</h2> */}
+    //     <div className="cards__container">
+    //         <div className="cards__wrapper">
+    //             <ul className="cards__items fade">
+    //             <CardItem 
+    //                 src={product.image}
+    //                 text={product.title}
+    //                 label="New!"
+    //                 path="/shop"
+    //                 />
+    //             </ul>
+    //         </div>
+    //     </div>
+    // </div>
+    
+    //))}
+    // </div> */}
