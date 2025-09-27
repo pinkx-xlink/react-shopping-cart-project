701487cc23959d935dd67c26394624d73a727118
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-27 16:00:09 -0400

    Add background image

--- shopping-cart/src/components/pages/Home.js
+++ shopping-cart/src/components/pages/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Welcome from '../Welcome'
 import Cards from '../Cards';
+import background from './stranger-background.jpg';
 
 function Home() {
     return (
         <>
             <Welcome />
             <Cards />
+            <style>{`
+            body {
+            background: url(${background}) no-repeat center center fixed;
+            background-size: cover;
+            }
+        `}
+        </style>
         </>
     )
 }
