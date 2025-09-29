6b5ac771f26a9efcc7ec50ceda39c2942d0dc451
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-29 16:24:38 -0400

    Add 'image-container' class to the second and third review cards' images

--- shopping-cart/src/components/Reviews.js
+++ shopping-cart/src/components/Reviews.js
@@ -18,13 +18,17 @@ const Reviews = () => {
             </div>
 
             <div className='review_card'>
-                <img src={image1} alt="Lady leaning on railing above the water"></img>
+                <div className='image-container'>
+                    <img src={image1} alt="Lady leaning on railing above the water"></img>
+                </div>
                 <h3>"Absolutely in love with this!"</h3>
                 <p>⭐⭐⭐⭐⭐</p>
                 <h4> - Rose </h4>
             </div>
             <div className='review_card'>
-                <img src={image1} alt="Lady leaning on railing above the water"></img>
+                <div className='image-container'>
+                    <img src={image1} alt="Lady leaning on railing above the water"></img>
+                </div>
                 <h3>"Absolutely in love with this!"</h3>
                 <p>⭐⭐⭐⭐⭐</p>
                 <h4> - Rose </h4>
