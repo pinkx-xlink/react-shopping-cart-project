07c4bb6f5c36bc517e66acc0bba15b1e6a26ae5d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-29 16:38:38 -0400

    Update images, reviews, and names for the second and third cards

--- shopping-cart/src/components/Reviews.js
+++ shopping-cart/src/components/Reviews.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './reviews.css';
 import image1 from './reviewImg/lady.jpg';
+import image2 from './reviewImg/lady1.jpg';
+import image3 from './reviewImg/guy.jpg';
 
 const Reviews = () => {
   return (
@@ -12,26 +14,26 @@ const Reviews = () => {
                     <img src={image1} alt="Lady leaning on railing above the water"></img>
                 </div>
                 
-                <h3>"Absolutely in love with this!"</h3>
+                <h3>"Absolutely in love with this FakeShop jacket!"</h3>
                 <p>⭐⭐⭐⭐⭐</p>
                 <h4> - Rose </h4>
             </div>
 
             <div className='review_card'>
                 <div className='image-container'>
-                    <img src={image1} alt="Lady leaning on railing above the water"></img>
+                    <img src={image2} alt="Lady leaning on railing above the water"></img>
                 </div>
-                <h3>"Absolutely in love with this!"</h3>
+                <h3>"I've been ordering from FakeShop for years. My favorite place to buy jewelry!"</h3>
                 <p>⭐⭐⭐⭐⭐</p>
-                <h4> - Rose </h4>
+                <h4> - Genevieve </h4>
             </div>
             <div className='review_card'>
                 <div className='image-container'>
-                    <img src={image1} alt="Lady leaning on railing above the water"></img>
+                    <img src={image3} alt="Lady leaning on railing above the water"></img>
                 </div>
-                <h3>"Absolutely in love with this!"</h3>
+                <h3>"The products are high quality and shipping is fast. I'll buy from FakeShop again!"</h3>
                 <p>⭐⭐⭐⭐⭐</p>
-                <h4> - Rose </h4>
+                <h4> - Joseph </h4>
             </div>
         </div>
 
