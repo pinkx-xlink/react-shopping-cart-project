9f2ac10ced716aa4a4b650d4d5453db34e9d259e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2025-09-29 20:59:16 -0400

    Rework the Reviews cards

--- shopping-cart/src/components/Reviews.js
+++ shopping-cart/src/components/Reviews.js
@@ -13,27 +13,32 @@ const Reviews = () => {
                 <div className='image-container'>
                     <img src={image1} alt="Lady leaning on railing above the water"></img>
                 </div>
-                
-                <h3>"Absolutely in love with this FakeShop jacket!"</h3>
-                <p>⭐⭐⭐⭐⭐</p>
-                <h4> - Rose </h4>
+                <div className="image-container_text">
+                    <h3>"Absolutely in love with this FakeShop jacket!"</h3>
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <h4> - Rose </h4>
+                </div>
             </div>
 
             <div className='review_card'>
                 <div className='image-container'>
                     <img src={image2} alt="Lady leaning on railing above the water"></img>
                 </div>
-                <h3>"I've been ordering from FakeShop for years. My favorite place to buy jewelry!"</h3>
-                <p>⭐⭐⭐⭐⭐</p>
-                <h4> - Genevieve </h4>
+                <div className="image-container_text">
+                    <h3>"I've been ordering from FakeShop for years. My favorite place to buy jewelry!"</h3>
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <h4> - Genevieve </h4>
+                </div>
             </div>
             <div className='review_card'>
                 <div className='image-container'>
                     <img src={image3} alt="Lady leaning on railing above the water"></img>
                 </div>
-                <h3>"The products are high quality and shipping is fast. I'll buy from FakeShop again!"</h3>
-                <p>⭐⭐⭐⭐⭐</p>
-                <h4> - Joseph </h4>
+                <div className='image-container_text'>
+                    <h3>"The products are high quality and shipping is fast. I'll buy from FakeShop again!"</h3>
+                    <p>⭐⭐⭐⭐⭐</p>
+                    <h4> - Joseph </h4>
+                </div>
             </div>
         </div>
 
